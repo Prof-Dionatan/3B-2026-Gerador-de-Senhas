c2b411894f407464c1f86d0a57c305ed4f9b1e1c
Prof-Dionatan <dionatan.markiu@escola.pr.gov.br>
2026-08-27 12:42:46 -0300

    campo senha

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto')
+const campoSenha = document.querySelector('#campo-senha')
 
+campoSenha.value = 'Aqui vai aparecer a senha.'
+
+let letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+// minusculas, numeros e simbolos
 
 tamanhoSenha = 8
 numeroSenha.textContent = tamanhoSenha;
@@ -7,10 +12,14 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao')
 
 
-
+// pega o 1º botão = -
 botoes[0].onclick = diminuir;
 
+// função diminuir
 function diminuir(){
+    // diminui de 1 em 1 e costra na tela
     tamanhoSenha--;
     numeroSenha.textContent = tamanhoSenha;
 }
+
+// BOTAO DE AUMENTAR
