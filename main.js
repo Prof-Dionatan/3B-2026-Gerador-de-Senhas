0806f1f09a4188b1977ca5add2f41250ea340b6f
Prof-Dionatan <dionatan.markiu@escola.pr.gov.br>
2026-09-03 12:44:11 -0300

    função de gerar a senha

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const numeroSenha = document.querySelector('.parametro-senha__texto')
 const campoSenha = document.querySelector('#campo-senha')
 
 campoSenha.value = 'Aqui vai aparecer a senha.'
-
+// OPEN IN WEBVIEW 
 let letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 // minusculas, numeros e simbolos
 
@@ -20,6 +20,22 @@ function diminuir(){
     // diminui de 1 em 1 e costra na tela
     tamanhoSenha--;
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha()
 }
 
 // BOTAO DE AUMENTAR
+
+
+// fUNÇÃO DE CRIAR A SENHA ALEATÓRIA
+geraSenha()
+
+function geraSenha(){
+    let senha = ''
+    // LOOP - Repetições
+    for(let i = 0; i < tamanhoSenha; i++){
+        let numeroAleatorio = Math.random() * 26;
+        numeroAleatorio = Math.floor(numeroAleatorio)
+        senha = senha + letrasMaiusculas[numeroAleatorio]
+    }
+    campoSenha.value = senha;
+}
